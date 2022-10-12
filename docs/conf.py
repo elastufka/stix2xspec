d6cfde5c5def8a203d90a7943ff0ab8d1f3e37a0
elastufka <elastufka@gmail.com>
2022-10-12 11:22:06 +0200

    try to get correct links

--- docs/conf.py
+++ docs/conf.py
@@ -19,12 +19,24 @@ sys.path.insert(0, os.path.abspath("../"))
 #sys.path.insert(0, os.path.abspath("../.."))
 #sys.path.insert(0, os.path.abspath("../stix2xspec"))
 
-extensions = ['sphinx.ext.duration', 'sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'sphinx.ext.autosummary', 'myst_parser']
+extensions = ['sphinx.ext.duration', 'sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'sphinx.ext.autosummary', 'myst_parser', 'sphinxcontrib.apidoc']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
+#from https://github.com/cherrypy/cheroot/blob/main/docs/conf.py
+master_doc = 'index'
 
+apidoc_excluded_paths = ['../stix2xspec/data']
+apidoc_extra_args = [
+    '--implicit-namespaces',
+    '--private',  # include “_private” modules
+]
+apidoc_module_dir = '../stix2xspec'
+apidoc_module_first = False
+#apidoc_output_dir = 'pkg'
+apidoc_separate_modules = True
+apidoc_toc_file = None
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
